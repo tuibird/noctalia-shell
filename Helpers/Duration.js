c62e199ea7e6fdf3a70f11acf069fc66adcd558e
quadbyte <studio@quadbyte.net>
2025-08-10 13:26:37 -0400

    Bringing back 2 helpers

---
+++ Helpers/Duration.js
@@ -0,0 +1,18 @@
+// Use to display the time remaining on the Battery widget
+function formatVagueHumanReadableDuration(totalSeconds) {
+    const hours = Math.floor(totalSeconds / 3600);
+    const minutes = Math.floor((totalSeconds - (hours * 3600)) / 60);
+    const seconds = totalSeconds - (hours * 3600) - (minutes * 60);
+ 
+    var str = "";
+    if (hours) {
+        str += hours.toString() + "h";
+    }
+    if (minutes) {
+        str += minutes.toString() + "m";
+    }
+    if (!hours && !minutes) {
+        str += seconds.toString() + "s";
+    }
+    return str;
+}
