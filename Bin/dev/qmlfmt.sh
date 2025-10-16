a431d87dc1a6e85c2409abef56f554d440428f0e
Kainoa Kanter <kainoa@t1c.dev>
2025-10-16 13:36:44 -0700

    fix: cd in precommit hook, not in qmlfmt script

--- Bin/dev/qmlfmt.sh
+++ Bin/dev/qmlfmt.sh
@@ -4,6 +4,5 @@
 # Can be installed from AUR "qmlfmt-git"
 # Requires qt6-5compat
 
-cd "$(git rev-parse --show-toplevel)"
 echo "Formatting $(find . -name "*.qml" | wc -l) files..."
 find . -name "*.qml" -print0 | xargs -0 -P "$(nproc)" -I {} qmlfmt -e -b 360 -t 2 -i 2 -w {}
